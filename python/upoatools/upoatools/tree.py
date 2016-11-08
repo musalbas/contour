a9a7b18a181e7d2480397ef4fc417211109c2996
Mustafa Al-Bassam <mus@musalbas.com>
2016-11-08 15:28:45 +0000

    Add function for importing merkle tree from JSON.

--- python/upoatools/upoatools/tree.py
+++ python/upoatools/upoatools/tree.py
@@ -23,3 +23,14 @@ def build_tree_from_directory(source_directory):
 
 def export_tree_as_json(mt):
     return json.dumps((mt.keys, mt.get_all_hex_chains()))
+
+
+def import_tree_from_json(json_string):
+    mt_json = json.loads(json_string)
+    mt = MerkleTree()
+    mt.key = mt_json[0]
+
+    for item in mt_json[1]:
+        mt.add_hash(item[0][0])
+
+    return mt
