8a6fba3b02a73be1eeb32344eea74fe4b09247ea
Mustafa Al-Bassam <mus@musalbas.com>
2017-01-05 13:54:18 +0000

    SPV base.

--- contourclient/contourclient/btcspv.py
+++ contourclient/contourclient/btcspv.py
@@ -1,1 +1,8 @@
 """Bitcoin SPV network operations."""
+
+
+class ContourSPV(object):
+    """Bitcoin SPV client for Contour operations."""
+    def __init__(self):
+        """Initialise SPV client."""
+        pass
