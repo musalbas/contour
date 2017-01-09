133df1d9613b196cd9f479737bb162597bc1c1a4
Mustafa Al-Bassam <mus@musalbas.com>
2017-01-09 16:37:27 +0000

    Verify API base.

--- contourclient/contourclient/api.py
+++ contourclient/contourclient/api.py
@@ -17,3 +17,14 @@ def sync(timeout=5, length_change_callback=None):
     bm.sync(timeout, length_change_callback)
 
     return bm.blockchain()
+
+
+def verify_inclusion_proof(proof):
+    """
+    Verifies an inclusion proof.
+
+    Args:
+        proof: the proof information tuple.
+    Returns:
+        A tuple where the first element is a boolean representing if the proof verifies, and the second element is the number of confirmations the proof's block has.
+    """
