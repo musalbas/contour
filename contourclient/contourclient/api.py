06ee725ebf707d16d32219f40a63eb32ce370fa4
Mustafa Al-Bassam <mus@musalbas.com>
2017-01-09 21:06:35 +0000

    Working merkle root comparison.

--- contourclient/contourclient/api.py
+++ contourclient/contourclient/api.py
@@ -1,5 +1,7 @@
 """API for Contour client."""
 
+from binascii import hexlify
+
 from merkle import check_hex_chain
 from pycoin.block import BlockHeader
 
@@ -31,6 +33,22 @@ def verify_inclusion_proof(proof):
     Returns:
         A tuple where the first element is a boolean representing if the proof verifies, the second element is the number of confirmations the proof's block has, and the third element is the hash being proved.
     """
-    # Check that the block is in the blockchain
+    # Get hash being proved for inclusion
+    hash_proving = proof[2][0][0]
+
+    # Check that the block is in the blockchain, and get confirmations
     blockheader = BlockHeader.from_bin(bytes.fromhex(proof[1][0]))
-    print(blockheader)
+    bm = BlockchainManager()
+    blockchain = bm.blockchain()
+    block_index = blockchain.index_for_hash(blockheader.hash())
+    if not block_index:
+        # Block not found in blockchain; fail verification
+        return (False, 0, hash_proving)
+    latest_block_index = blockchain.index_for_hash(blockchain.last_block_hash())
+    confirmations = latest_block_index - block_index
+
+    # Check that merkle root in the block merkle path matches the block
+    if hexlify(blockheader.merkle_root).decode() != proof[1][1][-1][0]:
+        return (False, confirmations, hash_proving)
+
+    return (True, confirmations, hash_proving)
